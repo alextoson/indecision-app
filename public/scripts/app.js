c67df41fe2853c9d76590a1439f78a85b0b3f0c6
Alex T <alex@toson.com.au>
2020-01-18 09:41:57 +1100

    Completed part 2 of ES6 Classes. It described how to extend an existing class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,8 +11,41 @@ class Person {
 	}
 }
 
-const me = new Person('Alex Toson', 26);
-console.log(me.getDescription());
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription() {
+		let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+		if (this.hasMajor()) {
+			description += ` Their major is ${this.major}.`;
+		}
+
+		return description;
+	}
+}
+
+class Traveller extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+	getGreeting() {
+		let greeting = super.getGreeting();
+		if (this.homeLocation) {
+			greeting += ` I'm visiting from ${this.homeLocation}!`;
+		}
+		return greeting;
+	}
+}
+
+const me = new Traveller('Alex Toson', 27, 'Sydney');
+console.log(me.getGreeting());
+
+const other = new Traveller();
+console.log(other.getGreeting());
