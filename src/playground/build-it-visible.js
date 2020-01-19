a3ad92eca712346ef62c05b95be4058d30d74bc7
Alex T <alex@toson.com.au>
2020-01-19 17:10:47 +1100

    converted the build-it example to be using state and setState, to get more practice in using state

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,22 +1,52 @@
-let visibility = false;
-
-const toggleText = () => {
-	visibility = !visibility;
-	render();
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visibility: false
+		}
+	}
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {
+				visibility: !prevState.visibility
+			};
+		});
+	}
+	render() {
+		return (
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
+				{this.state.visibility && ( 
+					<p>Hey. These are some details you can now see!</p> 
+				)}
+			</div>
+		);	
+	}
 }
 
-const appRoot = document.querySelector('#app');
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const render = () => {
-	const template = (
-		<div>
-			<h1>Visibility Toggle</h1>
-			<button onClick={toggleText}>{visibility ? 'Hide Details' : 'Show Details'}</button>
-			{visibility && <p>Hey. These are some details you can now see!</p>}
-		</div>
-	)
+// let visibility = false;
 
-	ReactDOM.render(template, appRoot);
-}
+// const toggleText = () => {
+// 	visibility = !visibility;
+// 	render();
+// }
 
-render();
+// const appRoot = document.querySelector('#app');
+
+// const render = () => {
+// 	const template = (
+// 		<div>
+// 			<h1>Visibility Toggle</h1>
+// 			<button onClick={toggleText}>{visibility ? 'Hide Details' : 'Show Details'}</button>
+// 			{visibility && <p>Hey. These are some details you can now see!</p>}
+// 		</div>
+// 	)
+
+// 	ReactDOM.render(template, appRoot);
+// }
+
+// render();
