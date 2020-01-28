10f1a52a17c2c756b5a390472415b1c01eb0c1f1
alextoson <alex@toson.com.au>
2020-01-28 14:29:27 +1100

    returned back to rendering indecision app

--- src/app.js
+++ src/app.js
@@ -1,24 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
-// import { ProgressPlugin } from "webpack";
 
-const Layout = props => {
-  return (
-    <div>
-      <p>header</p>
-      {props.children}
-      <p>footer</p>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Layout>
-    <div>
-      <h1>Page Title</h1>
-      <p>this is myt page</p>
-    </div>
-  </Layout>,
-  document.getElementById("app")
-);
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
