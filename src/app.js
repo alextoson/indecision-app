eac1949ae314de05eb5d1de6870f2d177a58cf7d
alextoson <alex@toson.com.au>
2020-01-28 14:28:29 +1100

    explored props.children method

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
+// import { ProgressPlugin } from "webpack";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = props => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Layout>
+    <div>
+      <h1>Page Title</h1>
+      <p>this is myt page</p>
+    </div>
+  </Layout>,
+  document.getElementById("app")
+);
